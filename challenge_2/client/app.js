a88c3d34e4916abf695152725b67bdef69965f96
Warren Tseng <wtseng678@gmail.com>
2018-07-10 19:07:33 -0700

    Implemented JSON to CSV parser

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,56 @@
+$(document).ready(function() {
+  
+  $("#form").on("submit", function(e){
+    e.preventDefault();
+    $.ajax({
+      url: "/", 
+      type: "POST",
+      data: $("textarea").val(),
+      success: function(result){
+        $("#csv").append('<span>' + result + '</span>');
+        // $.ajax({
+        //   url: "/",
+        //   type: "GET",
+        //   // data: {},
+        //   // more fields here
+        //   success: function(data) {
+        //     // render on the html
+        //     console.log(data);
+        //   }, 
+        //   error: function (data) {
+        //     console.error("failed to get");
+        //   }
+        // });
+      },
+      error: function(data) {
+        console.error("failed to post");
+      }
+    });
+  });
+});
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+const items = json3.items
+const replacer = (key, value) => value === null ? '' : value // specify how you want to handle null values here
+const header = Object.keys(items[0])
+let csv = items.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
+csv.unshift(header.join(','))
+csv = csv.join('\r\n')
+
+console.log(csv)
+
+*/
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,61 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static("client"));
+app.use(express.static("node_modules"));
 
 
+// app.get("/", function (req, res) {
+//   console.log("got");
+//   res.send("GET request to the homepage");
+// });
+
+var trim = function(obj) {
+  if (obj[obj.length - 1] === ';') {
+    obj = obj.slice(0, -1);
+  }
+  console.log(obj);
+  return JSON.parse(obj);
+}
+
+var rows = [];
+var parseObj = function(obj) {
+  var row = [];
+  for (var key in obj) {
+    if (Array.isArray(obj[key])) {
+      rows.push(row.join(","));
+      for (var i = 0; i < obj[key].length; i++) {
+        parseObj(obj[key][i]);
+      }
+    } else {
+      row.push(obj[key]);
+    }
+  }
+  return rows;
+};
+
+// POST method route
+app.post("/", function (req, res) {
+  var str = "";
+  req.on('data', function(data) {
+    var obj = trim(data.toString())
+    var keys = Object.keys(obj);
+    for (var key = 0; key < keys.length; key++) {
+      if (Array.isArray(obj[keys[key]])) {
+        keys.splice(key, key + 1);
+      }
+    }
+    var s = parseObj(trim(data.toString()));
+    str = keys.join(",");
+    for (var i = 0; i < s.length; i++) {
+      str += "\n" + s[i];
+    }
+  });
+  req.on('end', function(data) {
+    res.send(str);
+    rows = [];
+  });
+});
+
+
+app.listen(3000);
